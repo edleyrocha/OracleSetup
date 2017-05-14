a8cf68d5ac8f20293eeea4ec5c54a51368878feb
Edley Allan Hoyler <edleyrocha@hotmail.com>
2017-05-14 01:32:35 -0400

    Build 14
    Clean

--- Oracle11G/dbora/Modo2/scripts/ora_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# script to start the Oracle database, listener and dbconsole
-. ~/.bash_profile
-# start the listener and the database
-$ORACLE_HOME/bin/dbstart $ORACLE_HOME
-# start the Enterprise Manager db console
-$ORACLE_HOME/bin/emctl start dbconsole
-exit 0
--- Oracle11G/dbora/Modo2/scripts/ora_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# script to stop the Oracle database, listener and dbconsole
-. ~/.bash_profile
-# stop the Enterprise Manager db console
-$ORACLE_HOME/bin/emctl stop dbconsole
-# stop the listener and the database
-$ORACLE_HOME/bin/dbshut $ORACLE_HOME
-exit 0
