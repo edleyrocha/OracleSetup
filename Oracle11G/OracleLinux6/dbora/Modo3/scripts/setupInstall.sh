a032c96f98f8f114794cb5c1ac39394cc32da8bf
Edley <edleyrocha@hotmail.com>
2019-10-05 19:18:53 -0400

    Build 18
    Build 18

---
+++ Oracle11G/OracleLinux6/dbora/Modo3/scripts/setupInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+file="/etc/init.d/oracle"
+if [ -f "$file" ]
+then
+	chkconfig --del oracle 
+	mv /etc/init.d/oracle /etc/init.d/oracle.backup_$(date +%d-%m-%y__%H-%M-%S)
+	echo "$File Backup Old"
+	ln -s /home/oracle/scripts/oracle.sh /etc/init.d/oracle
+	echo "$File Link Create /etc/init.d/oracle "
+else
+	ln -s /home/oracle/scripts/oracle.sh /etc/init.d/oracle
+	echo "$File Link Create /etc/init.d/oracle "
+fi
+chmod 770 -R /home/oracle/scripts
+chown oracle:root -R /home/oracle/scripts
+chmod u+x /home/oracle/scripts/*.sh
+chkconfig --add oracle
+chkconfig --list
+exit 0
--- Oracle11G/dbora/OracleLinux7/Modo2/scripts/ora_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# script to stop the Oracle database, listener and dbconsole
-. ~/.bash_profile
-# stop the listener and the database
-$ORACLE_HOME/bin/dbshut $ORACLE_HOME
-# stop the Enterprise Manager db console
-$ORACLE_HOME/bin/emctl stop dbconsole
-exit 0
